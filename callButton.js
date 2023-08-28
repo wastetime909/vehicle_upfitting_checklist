bdf3da1cd9e75cdcb81ef2e117f144bb21ccbb1d
wastetime909 <wastetime909@gmail.com>
2023-08-28 10:03:49 -0400

    Finalized commit

--- callButton.js
+++ callButton.js
@@ -1,5 +1,7 @@
 function createModalButtonsFromArray(stringsArray) {
     const buttonContainer = document.getElementById('buttonContainer');
+
+    buttonContainer.innerHTML = "";
   
     stringsArray.forEach((stringValue, index) => {
       const newButton = document.createElement('button');
@@ -8,11 +10,10 @@ function createModalButtonsFromArray(stringsArray) {
       .replace(/\s+/g, '_')
       .replace(/(^|_)([a-z])/g, (match, p1, p2) => p2.toUpperCase());
       newButton.id = `button${index}`;
-      newButton.className = 'open-modal-button';
       newButton.setAttribute('data-image', `${jsFriendlyStringValue}.jpg`);
       newButton.setAttribute('data-text', `${jsFriendlyStringValue}`);
       newButton.textContent = `${stringValue}`;
-      newButton.setAttribute('class', `btn btn-light`);
+      newButton.setAttribute('class', `btn btn-info btn-lg m-3`);
   
       newButton.addEventListener('click', function() {
         const imageSource = this.getAttribute('data-image');
--- textContents.js
+++ textContents.js
@@ -9,14 +9,34 @@ It can also span multiple lines.
 `;
 
 window.FuseBlock = `
-This is the text content for Fuse Block.
-It can also span multiple lines.
+Fuse block is required if there's a need of electronic upfitting option
 `;
 
 window.MasterSwitch =`
-Placeholder for Master Switch
+Master switch to be placed inside the cabinet for high ampage upfitting devices such as power liftgate
 `;
 
 window.TwoHandedSwitch =`
-Placeholder for Two Handed Switch
+Two handed switch is required for operating power liftgate, which is to be installed at the curb side of the truck. Operator must use both hands to operate the power liftgate
+without being able to physically reach the liftgate during operation
+`;
+
+window.RoofRack =`
+Roof rack/roof bar must be used to mount lighting devices such as amber strobe, spot light. Extra attention must be paid to make sure no drilling to be made on Body In White
+`;
+
+window.BatteryBrain =`
+One (1) Programmable Battery Isolator shall be installed and monitor vehicle 12V battery system. Must be compatible with vehicle and options selected.
+Chassis wiring harnesses shall not be cut, tapped or spliced. Any connections to the chassis harness shall be made via connectors supplied by the chassis OEM. If available, OEM upfitter switches are to be used for options selected where applicable. 
+`;
+
+window.KeyStorageTube =`
+Permanently installed, GE Supra Elante (Part #001607-1) white solid metal key storage tube for secure and concealed spare ignition key storage
+`;
+
+window.BackupAlarm =`
+Multi-frequency broadband white sound backup alarm (e.g Brigade Model #SY82BB)
+`;
+window.BackRack =`
+
 `;
